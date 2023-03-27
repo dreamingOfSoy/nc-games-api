505756d253bb8c93982615ffc5d7c0c7b404dc88
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-27 12:32:37 +0100

    add test file for categories

---
+++ __tests__/categories.test.js
@@ -0,0 +1,7 @@
+const { getAllCategories } = require("../controllers/categoriesController");
+
+describe("getAllCategories()", () => {
+  it.todo(
+    "200 - should respond with correct status code and contain correct feilds (slug, description)"
+  );
+});
