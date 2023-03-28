908b644e77d926e16ca0bf7f74b9d0431546c801
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-28 15:37:12 +0100

    refactor to handle 404 error

--- errorHandlers/clientErrorHandler.js
+++ errorHandlers/clientErrorHandler.js
@@ -1,5 +1,11 @@
 exports.clientErrorHandler = (err, req, res, next) => {
-  if (err.status === 400) res.status(400).send(err);
+  if (
+    err.status === 400 &&
+    err.error === `${originalUrl} is not available on this server`
+  )
+    res.status(400).send(err);
+  if (err.status === 404 && err.error === "review not found")
+    res.status(404).send(err);
 
   next(err);
 };
