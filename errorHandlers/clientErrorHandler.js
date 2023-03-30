531da14fa668a840766b57f6cd85d226b01b1a87
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-30 14:06:12 +0100

    refactor error handler

--- errorHandlers/clientErrorHandler.js
+++ errorHandlers/clientErrorHandler.js
@@ -1,14 +1,25 @@
 exports.clientErrorHandler = (err, req, res, next) => {
   if (
     err.status === 400 &&
-    err.error === `${originalUrl} is not available on this server`
-  )
+    err.error === "Invalid input type for votes, input must be a number"
+  ) {
     res.status(400).send(err);
+  }
 
-  if (err.status === 404 && err.error === "review not found")
+  if (
+    err.status === 400 &&
+    err.error === `${originalUrl} is not available on this server`
+  ) {
+    res.status(400).send(err);
+  }
+
+  if (err.status === 404 && err.error === "review not found") {
     res.status(404).send(err);
+  }
 
-  if (err.status === 422) res.status(422).send(err);
+  if (err.status === 422) {
+    res.status(422).send(err);
+  }
 
   next(err);
 };
