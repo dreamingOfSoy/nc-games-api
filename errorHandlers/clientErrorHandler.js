79883eb69fd8188ef711017af4d9ac9285a07a5f
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-30 12:18:35 +0100

    refactor client error handler

--- errorHandlers/clientErrorHandler.js
+++ errorHandlers/clientErrorHandler.js
@@ -8,11 +8,7 @@ exports.clientErrorHandler = (err, req, res, next) => {
   if (err.status === 404 && err.error === "review not found")
     res.status(404).send(err);
 
-  if (
-    err.status === 422 &&
-    err.error === "A comment must have both a body and a username"
-  )
-    res.status(422).send(err);
+  if (err.status === 422) res.status(422).send(err);
 
   next(err);
 };
