23b13576f500c463728e896809d6bc81b24f5eed
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-30 17:24:56 +0100

    refactor for new error

--- errorHandlers/clientErrorHandler.js
+++ errorHandlers/clientErrorHandler.js
@@ -8,12 +8,26 @@ exports.clientErrorHandler = (err, req, res, next) => {
 
   if (
     err.status === 400 &&
+    err.error === "Order query can only be asc or desc"
+  ) {
+    res.status(400).send(err);
+  }
+
+  if (
+    err.status === 400 &&
     err.error === `${originalUrl} is not available on this server`
   ) {
     res.status(400).send(err);
   }
 
   if (err.status === 404 && err.error === "review not found") {
+    res.status(404).send(err);
+  }
+
+  if (
+    err.status === 404 &&
+    err.error === `No category with the name ${req.query.category}`
+  ) {
     res.status(404).send(err);
   }
 
