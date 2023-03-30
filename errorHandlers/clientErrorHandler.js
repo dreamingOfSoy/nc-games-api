4b53bc6acebd811ba6ffe094ea672b56d2488a22
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-30 10:25:57 +0100

    add new error handlers

--- errorHandlers/clientErrorHandler.js
+++ errorHandlers/clientErrorHandler.js
@@ -4,8 +4,15 @@ exports.clientErrorHandler = (err, req, res, next) => {
     err.error === `${originalUrl} is not available on this server`
   )
     res.status(400).send(err);
+
   if (err.status === 404 && err.error === "review not found")
     res.status(404).send(err);
+
+  if (
+    err.status === 422 &&
+    err.error === "A comment must have both a body and a username"
+  )
+    res.status(422).send(err);
 
   next(err);
 };
