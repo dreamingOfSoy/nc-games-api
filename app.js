9053f01e6c74f8ba6a3f0c3cf5b96deab9f50717
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-17 11:28:38 +0100

    allow cross origin resouce sharing

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ const { psqlErrorHandler } = require("./errorHandlers/psqlErrorHandler");
 const endpointsJson = require("./endpoints.json");
 
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
